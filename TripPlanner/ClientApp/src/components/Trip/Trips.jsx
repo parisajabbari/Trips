0cf3c35d5cd89309bdc2f50e5b412740533680e5
parisajabbari <59807156+parisajabbari@users.noreply.github.com>
2020-04-12 14:41:49 +1000

    React - Addign a table
    Adding a table to list all the trips

---
+++ TripPlanner/ClientApp/src/components/Trip/Trips.jsx
@@ -0,0 +1,62 @@
+import React, {Component} from 'react';
+
+export class Trips extends Component
+{
+    constructor(props){
+        super(props);
+    
+
+        this.state = {
+            trips : [],
+            loading : false     
+        }
+    }
+
+    RenderAllTripsTable(trips){
+        return(
+            <table className = "table table-striped">
+                <thead>
+                    <tr>
+                        <th>Name</th>
+                        <th>Description</th>
+                        <th>Date started</th>
+                        <th>Date completed</th>
+                        <th>Action</th>
+                    </tr>
+                </thead>
+
+                <tbody>
+                    <tr>
+                        <td>a</td>
+                        <td>a</td>
+                        <td>a</td>
+                        <td>a</td>
+                        <td> - </td>
+                    </tr>
+                </tbody>
+
+            </table>
+        )
+    }
+
+    render(){
+
+        let content = this.state.loading ? (
+            <p> 
+                <em>Loading...</em>
+            </p>
+        ) : (
+                this.RenderAllTripsTable(this.state.trips)
+        )
+
+        return(
+            <div>
+                <h1>All trips</h1>
+                <p>Here you can see all trips</p>
+                {content}
+            </div>
+
+        );
+    }
+    
+}
