09bf4c447ce44a24d044fc8eaa3fa574274f4035
parisajabbari <59807156+parisajabbari@users.noreply.github.com>
2020-04-19 15:34:52 +1000

    Redux - Added reducer

---
+++ TripPlanner/ClientApp/src/reducers/index.js
@@ -0,0 +1,9 @@
+import {combineReducers} from 'redux';
+import tripReducers from './tripReducers';
+
+const rootReducer = combineReducers({
+    trips: tripReducers
+
+});
+
+export default rootReducer;
